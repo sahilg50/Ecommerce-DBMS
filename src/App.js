de159b8966029656dac126c03119ce2b776644e1
Sahil Gupta <sahilgupta.312000@gmail.com>
2021-05-06 22:07:08 +0530

    ADDing Header Component and links to Homepage and ShopPage

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import HomePage from './components/HomePage';
 import { Route, Switch } from 'react-router-dom';
 import ShopPage from './components/ShopPage';
+import Header from './components/Header';
 
 function App() {
 	return (
 		<div className="App">
+			<Header />
 			<Switch>
 				<Route exact path="/" component={HomePage}></Route>
 				<Route path="/shop" component={ShopPage}></Route>
---
+++ src/components/Header.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { ReactComponent as Logo } from './crown.svg';
+
+function Header() {
+	return (
+		<HeaderContainer>
+			<LogoContainer>
+				<Link to="/">
+					<Logo src="./crown.svg" />
+				</Link>
+			</LogoContainer>
+			<OptionsContainer>
+				<Option>
+					<Link to="/shop">SHOP</Link>
+				</Option>
+				<Option>
+					<Link to="/shop">CONTACT</Link>
+				</Option>
+			</OptionsContainer>
+		</HeaderContainer>
+	);
+}
+
+export default Header;
+
+const HeaderContainer = styled.div`
+	height: 70px;
+	width: 100%;
+	display: flex;
+	justify-content: space-between;
+	margin-bottom: 25px;
+`;
+
+const LogoContainer = styled.div`
+	height: 100%;
+	width: 70px;
+	padding: 25px;
+`;
+
+const OptionsContainer = styled.div`
+	width: 50%;
+	height: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: flex-end;
+`;
+
+const Option = styled.div`
+	padding: 10px 15px;
+`;
