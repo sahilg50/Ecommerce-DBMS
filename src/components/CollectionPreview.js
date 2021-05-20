2a31dff2ced005eb8ad605eb78499b7d017b5f8f
Sahil <56653026+sahilg50@users.noreply.github.com>
2021-05-20 13:25:43 +0530

    CheckPoint 2

--- src/components/CollectionPreview.js
+++ src/components/CollectionPreview.js
@@ -1,20 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import CollectionItem from './CollectionItem';
 import { withRouter } from 'react-router-dom';
+import axios from 'axios';
 
-const CollectionPreview = ({ title, items, history, match }) => {
+const CollectionPreview = ({ categoryName, history, match }) => {
+	const [fetched_Collection, setFetched_Collection] = useState([]);
+
+	const Fetch_Collections = async (categoryName) => {
+		try {
+			const response = await axios({
+				method: 'post',
+				url: 'http://localhost:4000/collections',
+				data: {
+					collection: categoryName,
+				},
+				responseType: 'json',
+			});
+			setFetched_Collection(response.data);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	useEffect(() => {
+		Fetch_Collections(categoryName);
+	}, [categoryName]);
+
 	return (
 		<CollectionPreviewContainer>
-			<TitleContainer onClick={() => history.push(`${match.url}${title}`)}>
-				{title.toUpperCase()}
+			<TitleContainer
+				onClick={() => history.push(`${match.url}/${categoryName}`)}
+			>
+				{categoryName.toUpperCase()}
 			</TitleContainer>
 			<PreviewContainer>
-				{items
-					.filter((item, idx) => idx < 5)
-					.map((item) => (
-						<CollectionItem key={item.id} item={item} />
-					))}
+				{fetched_Collection
+					.filter((fetched_item, idx) => idx < 5)
+					.map((fetched_item) => {
+						const item = {
+							name: fetched_item.productName,
+							id: fetched_item.productId,
+							imageUrl: fetched_item.productImage,
+							price: fetched_item.productPrice,
+						};
+						return <CollectionItem key={item.id} item={item} />;
+					})}
 			</PreviewContainer>
 		</CollectionPreviewContainer>
 	);
--- src/components/collectionOverview.js
+++ src/components/collectionOverview.js
@@ -1,22 +1,25 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 import styled from 'styled-components';
-import { selectCollectionForPreview } from '../redux/shop/shop.reducer';
+// import { selectCollectionForPreview } from '../redux/shop/shop.reducer';
 import CollectionPreview from './CollectionPreview';
 
 const CollectionOverview = () => {
-	const [total_categories, setTotal_categories] = useState({});
+	const [total_categories, setTotal_categories] = useState([]);
 
-	const collections = useSelector(selectCollectionForPreview);
+	// const collections = useSelector(selectCollectionForPreview);
+
 	const Fetch_Total_Number_of_Categories = async () => {
 		try {
-			const response = await axios.get(
-				`http://localhost:4000/total_categories`
-			);
+			const response = await axios({
+				method: 'get',
+				url: `http://localhost:4000/total_categories`,
+				responseType: 'json',
+			});
 			setTotal_categories(response.data);
 		} catch (error) {
-			console.log('Directory request error');
+			console.log('total categories request error');
 		}
 	};
 
@@ -24,10 +27,19 @@ const CollectionOverview = () => {
 		Fetch_Total_Number_of_Categories();
 	}, []);
 
+	// const total_categoriess = [
+	// 	{ category_id: '1', categoryName: 'hats' },
+	// 	{ category_id: '2', categoryName: 'jackets' },
+	// 	{ category_id: '3', categoryName: 'sneakers' },
+	// 	{ category_id: '4', categoryName: 'womens' },
+	// 	{ category_id: '5', categoryName: 'mens' },
+	// 	{ category_id: '6', categoryName: 'bags' },
+	// ];
+
 	return (
 		<CollectionsOverviewContainer>
-			{collections.map(({ id, ...otherCollectionProps }) => (
-				<CollectionPreview key={id} {...otherCollectionProps} />
+			{total_categories.map(({ category_id, categoryName }) => (
+				<CollectionPreview key={category_id} categoryName={categoryName} />
 			))}
 		</CollectionsOverviewContainer>
 	);
